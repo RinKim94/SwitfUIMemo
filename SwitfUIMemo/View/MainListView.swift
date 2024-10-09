e77276501d0af241d18b19b2f7f8a7e209d92e32
bunabloom <kimrinoffdev@gmail.com>
2024-10-10 08:14:53 +0900

    feat) Add Sheet

--- SwitfUIMemo/View/MainListView.swift
+++ SwitfUIMemo/View/MainListView.swift
@@ -15,6 +15,7 @@ struct MainListView: View {
     //뷰가 생성되기 직전에 공유데이터 목록을 확인하고 스토어의 타입과 동일한 타입의 인스턴스가 등록되어 있다면 속성을 해당 인스턴스로 자동으로 초기화함
     //하나의 데이터를 여러 view에서 공유하고싶을때
     @EnvironmentObject var store: MemoStore
+    @State private var showComposer: Bool = false
     
     var body: some View {
         NavigationView {
@@ -25,6 +26,16 @@ struct MainListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
+            .toolbar {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showComposer) {
+                ComposeView()
+            }
         }
     }
 }

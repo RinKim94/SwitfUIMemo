6e6bb69b7e6f0eaaa9deeb2ff5bac4951616b515
bunabloom <kimrinoffdev@gmail.com>
2024-10-10 06:46:50 +0900

    feat) add composeView

---
+++ SwitfUIMemo/View/ComposeView.swift
@@ -0,0 +1,50 @@
+//
+//  ComposeView.swift
+//  SwitfUIMemo
+//
+//  Created by KimRin on 10/10/24.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store: MemoStore
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var content: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("새 메모")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement:
+                        .navigationBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("취소")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        store.insert(memo: content)
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ComposeView()
+        .environmentObject(MemoStore())
+}
